25e377b90cd6b1676e023f34bd0726dd29b279b4
Prem Yadav <yp36yadav@gmail.com>
2026-01-21 12:20:46 +0530

    Fix swiper pagination css import

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
-import "swiper/css/pagination";
+
 
 import { useRouter } from "next/navigation"; // ✅ CORRECT IMPORT
 
